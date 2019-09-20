275015ebd7585b26b2d6e0687b94dfc75e33d9a8
Jimmy Yen <zeng.tw@gmail.com>
2019-09-20 22:58:31 +0800

    a modify suggestion for #3946

--- resources/views/filter/where.blade.php
+++ resources/views/filter/where.blade.php
@@ -1,6 +1,7 @@
 <div class="form-group">
-    <label class="col-sm-2 control-label"> {{$label}}</label>
-    <div class="col-sm-8">
+    <label class="col-md-12 hidden-sm"> {{$label}}</label>
+    <label class="col-sm-2 hidden visible-sm control-label"> {{$label}}</label>
+    <div class="col-sm-8 col-md-12">
         @include($presenter->view())
     </div>
 </div>
